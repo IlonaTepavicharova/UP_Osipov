6cfb7e05b96334331e1ab1fb9d0784a2eb79d3ae
Ilona <ilonatepavicharova@gmail.com>
2025-09-24 14:48:39 +0300

    Меню на основе аутентификации

--- scripts/menu.php
+++ scripts/menu.php
@@ -1,8 +1,13 @@
 <div id="menu">
     <ul>
-        <li><a href="../index.php">Главная страница</a></li>
-        <li><a href="/scripts/show-user.php">Мой профиль</a></li>
-        <li><a href="/scripts/logout.php">Отмена авторизации</a></li>
-        <li><a href="/scripts/login.php">Авторизация</a></li>
+        <li><a href="/index.html">Главная страница</a></li>
+        <?php
+        if (isset($_COOKIE["username"])) {
+            echo "<li><a href='/scripts/show-user.php?user_id=" . $_COOKIE["user_id"] . "'>Мой профиль</a></li>";
+            echo "<li><a href='/scripts/logout.php'>Отмена авторизации</a></li>";
+        } else {
+            echo "<li><a href='/scripts/login.php'>Авторизация</a></li>";
+        }
+        ?>
     </ul>
 </div>
