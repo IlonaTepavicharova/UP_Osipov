6b765f0eca3b6d4fe83b98bb1288536536261867
Ilona <ilonatepavicharova@gmail.com>
2025-09-24 14:50:18 +0300

    Ссылка на редактирование профиля

--- scripts/show-user.php
+++ scripts/show-user.php
@@ -1,21 +1,21 @@
 <?php
 require("connect.php");
-require("authorize.php"); 
+require("authorize.php");
 // извлекаем данные из суперглобального массива REQUEST 
 $user_id = $_REQUEST["user_id"];
 // создание строки инструкции SELECT 
-$select_query = "SELECT * FROM `users` WHERE `user_id` = " . $user_id; 
-$res = $mysqli->query($select_query); 
-if ($res) { 
-$row = $res->fetch_array(); 
-$first_name = $row['first_name']; 
-$last_name = $row['last_name']; 
-// сценарий может быть указан в теге источника изображения - src 
-$user_image = "show-image.php?user_id=" . $user_id; 
-$bio = preg_replace('/[\r\n]+/', '<p>', $row['bio']); 
-$email = $row['email']; 
-$url_site = $row['url_site']; 
-$vk = $row['vk']; 
+$select_query = "SELECT * FROM `users` WHERE `user_id` = " . $user_id;
+$res = $mysqli->query($select_query);
+if ($res) {
+    $row = $res->fetch_array();
+    $first_name = $row['first_name'];
+    $last_name = $row['last_name'];
+    // сценарий может быть указан в теге источника изображения - src 
+    $user_image = "show-image.php?user_id=" . $user_id;
+    $bio = preg_replace('/[\r\n]+/', '<p>', $row['bio']);
+    $email = $row['email'];
+    $url_site = $row['url_site'];
+    $vk = $row['vk'];
 } else {
     header("Location: show-error.php?error_message=Ошибка получения пользователя с ID = $user_id&system_error_message=Невозможно обработать запрос на извлечение данных пользователя");
     exit;
@@ -58,20 +58,21 @@ $vk = $row['vk'];
                     $first_name; ?>:</p>
                 <ul>
                     <li>...по электронной почте
-                        <a href="<?php echo $email; ?>">
-                            <?php echo $email;
-                            ?>
-                        </a>
+                        <a href="<?php echo $email; ?>"><?php echo $email; ?></a>
                     </li>
                     <li>...путем
-                        <a href="<?php echo $url_site; ?>">посещения его
-                            сайта</a>
+                        <a href="<?php echo $url_site; ?>">посещения его сайта</a>
                     </li>
                     <li>...путем
-                        <a href="<?php echo $vk; ?>">отслеживания его сообщений
-                            в VK</a>
+                        <a href="<?php echo $vk; ?>">отслеживания его сообщений в VK</a>
                     </li>
                 </ul>
+                <hr />
+                <p>
+                <form method='POST' action='/scripts/edit-user.php?user_id= <?php echo $user_id; ?>'>
+                    <button type='submit'>Редактировать</button>
+                </form>
+                </p>
             </div>
         </div>
         <div id="footer"></div>
