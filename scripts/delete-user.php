dc4391dd15a11deb79fcfc1a79f6fca61964fd08
Ilona <ilonatepavicharova@gmail.com>
2025-09-24 13:10:35 +0300

    Удаление пользователя

---
+++ scripts/delete-user.php
@@ -0,0 +1,28 @@
+<?php
+// подключение к серверу 
+require("connect.php");
+$user_id = $_REQUEST['user_id'];
+// создание sql инструкции на удаление записи таблицы images 
+$delete_images_sql = sprintf("DELETE FROM `images` WHERE `user_id` 
+= %d", $user_id);
+// выполнение запроса 
+$res = $mysqli->query($delete_images_sql);
+// если запрос на удаление удачен 
+// переходим к удалению записи таблицы users 
+if ($res) {
+    $delete_users_sql = sprintf("DELETE FROM `users` WHERE `user_id` 
+= %d", $user_id);
+    $res = $mysqli->query($delete_users_sql);
+    if ($res) {
+        // возвращаем пользователя к исходному сценарию 
+        header("Location: show-users.php");
+    } else {
+        header("Location: show-error.php?error_message=Ошибка 
+удаления выбранного пользователя&system_error_message=" .
+            $mysqli->error);
+    }
+} else {
+    header("Location: show-error.php?error_message=Ошибка удаления 
+изображения пользователя&system_error_message=" . $mysqli->error);
+}
+exit;
---
+++ scripts/show-users.php
@@ -0,0 +1,56 @@
+<?php
+// подключение к серверу 
+require("connect.php");
+// создание строки инструкции SELECT 
+$select_sql = "SELECT `user_id`, `last_name`, `first_name`, `email` 
+FROM `users`";
+// выполнение запроса 
+$res = $mysqli->query($select_sql);
+// если запрос выполнен выводим список пользователей 
+if ($res) {
+    while ($row = $res->fetch_array()) {
+        $html .= sprintf(
+            "<li> 
+<a href='show-user.php?user_id=%d'>%s %s</a> 
+(<a href='mailto:%s'>%s</a>) 
+<a href='delete-user.php?user_id=%d'><img 
+class='delete_user' src='../images/delete.png' width=15px 
+/></a> 
+</li>",
+            $row['user_id'],
+            $row['first_name'],
+            $row['last_name'],
+            $row['email'],
+            $row['email'],
+            $row['user_id']
+        );
+    }
+} else {
+    header("Location: show-error.php?error_message=Ошибка вывода 
+списка пользователей&system_error_message=" . $mysqli->error);
+    exit;
+}
+?>
+<html>
+
+<head>
+    <link href="../css/phpMM.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+    <div class="wrap">
+        <div id="header">
+            <h1>PHP & MySQL: The Missing
+                Manual</h1>
+        </div>
+        <div id="example">Список</div>
+
+            <?php require("menu.php"); ?>
+            <div id="content">
+                <?php echo $html; ?>
+            </div>
+            <div id="footer"></div>
+    </div>
+</body>
+
+</html>
